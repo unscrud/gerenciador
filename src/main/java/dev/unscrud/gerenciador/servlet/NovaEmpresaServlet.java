411d8e53c0a76e92a10bf7e68dd4525fba513ef5
Daniel Carvalho <daniel.devop@gmail.com>
2023-08-07 13:43:39 -0300

    servlet para criaçção de nova empresa

---
+++ src/main/java/dev/unscrud/gerenciador/servlet/NovaEmpresaServlet.java
@@ -0,0 +1,48 @@
+package dev.unscrud.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "NovaEmpresaServlet", urlPatterns = {"/novaEmpresa"})
+public class NovaEmpresaServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Cadastrando nova emrpesa");
+        String name = req.getParameter("nome");
+        processRequest(req, resp, name);
+    }
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @param name
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response, String name)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet NovaEmpresaServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet NovaEmpresaServlet at " + request.getContextPath() + "</h1>");
+            out.println("<p>The name "+name+" came in the request!</p>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+}
