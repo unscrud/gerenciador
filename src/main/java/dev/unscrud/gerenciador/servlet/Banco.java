5ca01a8ec13eb84ebb345f52fcc44710050bdf99
Daniel Carvalho <daniel.devop@gmail.com>
2023-08-10 09:22:39 -0300

    criação de empresas de exemplo

--- src/main/java/dev/unscrud/gerenciador/servlet/Banco.java
+++ src/main/java/dev/unscrud/gerenciador/servlet/Banco.java
@@ -4,9 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Banco {
-    
     private static List<Empresa> empresas = new ArrayList<>();
 
+    static {
+        empresas.add(new Empresa("Caelum"));
+        empresas.add(new Empresa("Alura"));
+        empresas.add(new Empresa("Google"));
+        empresas.add(new Empresa("Microsoft"));
+    }
+    
     public void adiciona(Empresa empresa) {
         empresas.add(empresa);
     }
