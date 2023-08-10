675baf8c0710cd0ee4cfba09ffe868e7ae8d2e7d
Daniel Carvalho <daniel.devop@gmail.com>
2023-08-10 18:15:38 -0300

    empresas de exemplo

--- src/main/java/dev/unscrud/gerenciador/servlet/Banco.java
+++ src/main/java/dev/unscrud/gerenciador/servlet/Banco.java
@@ -7,10 +7,10 @@ class Banco {
     private static List<Empresa> empresas = new ArrayList<>();
 
     static {
-        empresas.add(new Empresa("Caelum"));
-        empresas.add(new Empresa("Alura"));
-        empresas.add(new Empresa("Google"));
-        empresas.add(new Empresa("Microsoft"));
+        empresas.add(new Empresa("Caelum", null));
+        empresas.add(new Empresa("Alura", null));
+        empresas.add(new Empresa("Google", null));
+        empresas.add(new Empresa("Microsoft", null));
     }
     
     public void adiciona(Empresa empresa) {
