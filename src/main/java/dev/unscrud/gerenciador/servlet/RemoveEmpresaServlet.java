5959cc912d2bdbf946c4d34364a08bd90d12a52c
Daniel Carvalho <daniel.devop@gmail.com>
2023-08-11 09:26:44 -0300

    doGet pois o acesso é por hiperlink

--- src/main/java/dev/unscrud/gerenciador/servlet/RemoveEmpresaServlet.java
+++ src/main/java/dev/unscrud/gerenciador/servlet/RemoveEmpresaServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "RemoveEmpresaServlet", urlPatterns = {"/removeEmpresa"})
 public class RemoveEmpresaServlet extends HttpServlet {
 @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Integer id = Integer.valueOf(req.getParameter("idDaEmpresa"));
         
         Banco banco = new Banco();
