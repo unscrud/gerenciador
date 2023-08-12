1eed25aa59ee8149e5c10a82fcd39400e76eb604
Daniel Carvalho <daniel.devop@gmail.com>
2023-08-12 18:54:39 -0300

    prefixo de url para classes das ações

--- src/main/java/dev/unscrud/gerenciador/util/Constantes.java
+++ src/main/java/dev/unscrud/gerenciador/util/Constantes.java
@@ -2,4 +2,7 @@ package dev.unscrud.gerenciador.util;
 
 public final class Constantes {
     public static final String PREFIXO_URL_JSP = "WEB-INF/view/";
+    
+    public static final String PACOTE_ACOES = "dev.unscrud.gerenciador.acao.";
+    public static String ACOES_EMPRESAS = PACOTE_ACOES + "empresa.";
 }
