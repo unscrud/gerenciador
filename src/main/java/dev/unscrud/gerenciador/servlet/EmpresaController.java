47b35f83329b86e70ea5892a0ccce19d137405d6
Daniel Carvalho <daniel.devop@gmail.com>
2023-08-12 19:35:28 -0300

    capitalizando o parametro ação para corresponder ao nome da classe

--- src/main/java/dev/unscrud/gerenciador/servlet/EmpresaController.java
+++ src/main/java/dev/unscrud/gerenciador/servlet/EmpresaController.java
@@ -2,6 +2,7 @@ package dev.unscrud.gerenciador.servlet;
 
 import dev.unscrud.gerenciador.acao.Acao;
 import dev.unscrud.gerenciador.util.Constantes;
+import dev.unscrud.gerenciador.util.StringUtil;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,18 +14,23 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "empresas", urlPatterns = {"/empresas"})
 public class EmpresaController extends HttpServlet {
+    
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String parametroAcao = request.getParameter("acao");
+    protected void service(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        
+        String parametroAcao = StringUtil
+                .CapitalizarPrimeiraLetra(request.getParameter("acao"));
+        
         String nomeDaClasse = Constantes.ACOES_EMPRESAS + parametroAcao;
         
         try {
             Class classe = Class.forName(nomeDaClasse);
-            Object obj = classe.newInstance();
-            Acao acao = (Acao) obj;
+            Acao acao = (Acao) classe.newInstance();
             acao.executar(request, response);
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
             Logger.getLogger(EmpresaController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
 }
