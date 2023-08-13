dd2f5f51264a137b34975e3ed721abb6c8323b42
Daniel Carvalho <daniel.devop@gmail.com>
2023-08-13 12:36:28 -0300

    adicionando usuários diretamente no banco fake

--- src/main/java/dev/unscrud/gerenciador/modelo/Banco.java
+++ src/main/java/dev/unscrud/gerenciador/modelo/Banco.java
@@ -7,9 +7,12 @@ import java.util.function.Predicate;
 
 public class Banco {
     private static final List<Empresa> empresas = new ArrayList<>();
+    private static final List<Usuario> usuarios = new ArrayList<>();
     private static Integer chaveSequencial = 1;
 
     static {
+        Banco.usuarios.add(new Usuario("unscrud","unscrud123"));
+        Banco.usuarios.add(new Usuario("fulano","fulano123"));
         Banco.empresas.add(new Empresa(Banco.chaveSequencial++, "Caelum", null));
         Banco.empresas.add(new Empresa(Banco.chaveSequencial++, "Alura", null));
         Banco.empresas.add(new Empresa(Banco.chaveSequencial++, "Google", null));
