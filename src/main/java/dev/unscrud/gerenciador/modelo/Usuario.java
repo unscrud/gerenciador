7e739d75369ac7ba532e428eaf571e275d811548
Daniel Carvalho <daniel.devop@gmail.com>
2023-08-13 12:28:34 -0300

    representação de um usuário no sistema

---
+++ src/main/java/dev/unscrud/gerenciador/modelo/Usuario.java
@@ -0,0 +1,48 @@
+package dev.unscrud.gerenciador.modelo;
+
+import java.util.Objects;
+
+public class Usuario {
+    private String login;
+    private String senha;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Usuario other = (Usuario) obj;
+        if (!Objects.equals(this.login, other.login)) {
+            return false;
+        }
+        return Objects.equals(this.senha, other.senha);
+    }
+}
