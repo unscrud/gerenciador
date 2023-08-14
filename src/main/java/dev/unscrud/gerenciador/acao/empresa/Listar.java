b47262348214093abb57c47f5a23422c65e0df3b
Daniel Carvalho <daniel.devop@gmail.com>
2023-08-14 19:02:02 -0300

    permitindo gerar a lista em xml ou json

--- src/main/java/dev/unscrud/gerenciador/acao/empresa/Listar.java
+++ src/main/java/dev/unscrud/gerenciador/acao/empresa/Listar.java
@@ -1,5 +1,7 @@
 package dev.unscrud.gerenciador.acao.empresa;
 
+import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
 import dev.unscrud.gerenciador.acao.Acao;
 import dev.unscrud.gerenciador.modelo.Banco;
 import dev.unscrud.gerenciador.modelo.Empresa;
@@ -11,16 +13,41 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Listar implements Acao {
+    
+    private enum Formatos { JSON, XML }
+    
     @Override
     public void executar(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
                 Banco banco = new Banco();
         List<Empresa> empresas = banco.getEmpresas();
         
-        request.setAttribute("empresas", empresas);
+        String formato = request.getParameter("format");
         
-        request
-            .getRequestDispatcher(Constantes.PREFIXO_URL_JSP + "listaEmpresas.jsp")
-            .forward(request, response);
+        if (formato == null) {
+            request.setAttribute("empresas", empresas);
+            request
+                .getRequestDispatcher(Constantes.PREFIXO_URL_JSP + "listaEmpresas.jsp")
+                .forward(request, response);
+        } else {
+            String content = "";
+            String contentType = "";
+            switch (Formatos.valueOf(formato.toUpperCase())) {
+                case JSON:
+                    Gson gson = new Gson();
+                    content = gson.toJson(empresas);
+                    contentType = "application/json";
+                    break;
+                case XML:
+                    XStream xStream = new XStream();
+                    xStream.alias("empresa", Empresa.class);
+                    content = xStream.toXML(empresas);
+                    contentType = "application/xml";
+                    break;
+            }
+            
+            response.setContentType(contentType);
+            response.getWriter().print(content);
+        }
     }
 }
